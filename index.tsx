18deed8f2486a1740836bf37356aaea66c2bec10
Nick.Steffen <Nick.Steffen@nicknet.us>
2025-12-27 23:10:37 +0000

    Add Firebase registration

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './services/firebase';
 
 const container = document.getElementById('root');
 if (container) {
